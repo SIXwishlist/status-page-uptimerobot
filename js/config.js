f75e0b49e51675b41fcd1122f2147c038bf0e3c6
Lucas <lordapo@outlook.com>
2016-06-05 13:48:52 +0200

    First commit

---
+++ js/config.js
@@ -0,0 +1,11 @@
+// array of Monitor-specific API keys or Main API key to list all monitors
+var __apiKeys = [
+	'm777827608-d7069196ae154bb38ae9c3db', //KS1
+	'm777827604-27481273da459b52855c7615', //Lord-Apo.FR
+	'm777827614-33f9e7bd28672e666ebed1d2', //MC	
+	'm777827605-266f3e153f47546509d5ea13', //Seedbox
+	'm777827606-8a286ddf058d9114af68f636', //Transmission
+	'm777827607-efa78470782e524cfd11ee6e', //Plex
+];
+// refresh interval (in seconds)
+var __refresh = 60;
